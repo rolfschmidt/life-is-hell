304d281433eb8cf281d1ac1795eca1f3cc69cfc0
Rolf Schmidt <rolf@r-schmidt-gmbh.de>
2019-10-05 22:15:24 +0200

    Removed global variables.

--- game/scene_level1.js
+++ game/scene_level1.js
@@ -15,6 +15,9 @@ class SceneLevel1 extends Phaser.Scene {
         this.load.image('level1_bomb', './assets/bomb.png');
         this.load.spritesheet('level1_dude', './assets/dude.png', { frameWidth: 32, frameHeight: 48 });
         this.load.audio('level1_music', './sounds/level1.mp3');
+
+        this.scoreCount = 0;
+        this.gameOver = false;
     }
 
     create() {
@@ -35,202 +38,200 @@ class SceneLevel1 extends Phaser.Scene {
             this.add.image(backgroundX - 512, 384, 'level1_sky');
         }
 
-        //  The platforms group contains the ground and the 2 ledges we can jump on
-        platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
 
         for (var groundX = 420; groundX < 920; groundX = groundX + 32) {
             var blockType = 'level1_block_middle';
             if (groundX == 420) blockType = 'level1_block_left';
             if (groundX == 900) blockType = 'level1_block_right';
-            platforms.create(groundX, 400, blockType);
+            this.platforms.create(groundX, 400, blockType);
         }
         for (var groundX = 0; groundX < 320; groundX = groundX + 32) {
             var blockType = 'level1_block_middle';
             if (groundX == 0) blockType = 'level1_block_left';
             if (groundX == 320 - 32) blockType = 'level1_block_right';
-            platforms.create(groundX, 250, blockType);
+            this.platforms.create(groundX, 250, blockType);
         }
         for (var groundX = 750; groundX < 1070; groundX = groundX + 32) {
             var blockType = 'level1_block_middle';
             if (groundX == 750) blockType = 'level1_block_left';
             if (groundX == 1070 - 32) blockType = 'level1_block_right';
-            platforms.create(groundX, 220, blockType);
+            this.platforms.create(groundX, 220, blockType);
         }
         for (var groundX = 0; groundX < config.physics.arcade.width; groundX = groundX + 32) {
             var blockType = 'level1_block_middle';
             if (groundX == 0) blockType = 'level1_block_left';
             if (groundX == config.physics.arcade.width - 32) blockType = 'level1_block_right';
-            platforms.create(groundX, 568, blockType);
+            this.platforms.create(groundX, 568, blockType);
         }
 
         var bossDoors = this.physics.add.group();
         var bossDoor = bossDoors.create(940, 156, 'level1_boss_door');
         bossDoor.body.moves = false;
 
-        // The player and its settings
-        player = this.physics.add.sprite(100, 450, 'sprites', 'stand_right');
+        // The this.player and its settings
+        this.player = this.physics.add.sprite(100, 450, 'sprites', 'stand_right');
 
-        player.lastDirection = 'right';
+        this.player.lastDirection = 'right';
 
-        //  Player physics properties. Give the little guy a slight bounce.
-        player.setBounce(0.2);
+        //  this.Player physics properties. Give the little guy a slight bounce.
+        this.player.setBounce(0.2);
 
         // disable bounds because we want to loop the background
-        player.setCollideWorldBounds(true);
+        this.player.setCollideWorldBounds(true);
 
-        // player default values
-        player.godMode             = false;
-        player.godModeMoveSpeed    = 3;
-        player.godModeZoom         = 1;
-        player.store               = {};
-        player.store.velocityY     = 0;
-        player.store.jumpVelocityY = 0;
-        player.store.jumpCount     = 2;
-        player.store.jumpPossible  = true;
+        // this.player default values
+        this.player.godMode             = false;
+        this.player.godModeMoveSpeed    = 3;
+        this.player.godModeZoom         = 1;
+        this.player.store               = {};
+        this.player.store.velocityY     = 0;
+        this.player.store.jumpVelocityY = 0;
+        this.player.store.jumpCount     = 2;
+        this.player.store.jumpPossible  = true;
 
         //  Input Events
-        cursors     = this.input.keyboard.createCursorKeys();
-        cursors.W   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-        cursors.A   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-        cursors.S   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
-        cursors.D   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-        cursors.F9  = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F9);
-        cursors.F10 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F10);
+        this.cursors     = this.input.keyboard.createCursorKeys();
+        this.cursors.W   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.cursors.A   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.cursors.S   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.cursors.D   = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+        this.cursors.F9  = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F9);
+        this.cursors.F10 = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F10);
 
-        //  Some stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
-        stars = this.physics.add.group({
+        //  Some this.stars to collect, 12 in total, evenly spaced 70 pixels apart along the x axis
+        this.stars = this.physics.add.group({
             key: 'level1_star',
             repeat: 11,
             setXY: { x: 12, y: 0, stepX: 70 }
         });
 
-        stars.children.iterate(function (child) {
+        this.stars.children.iterate(function (child) {
 
             //  Give each star a slightly different bounce
             child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
 
         });
 
-        traps = this.physics.add.staticGroup();
-        traps.create(940, 567, 'level1_block_kill');
-        traps.create(940 + 32, 567, 'level1_block_lava');
-        traps.create(940 + 64, 567, 'level1_block_lava');
-        traps.create(940 + 96, 567, 'level1_block_lava');
-        traps.create(940 + 128, 567, 'level1_block_kill');
+        this.traps = this.physics.add.staticGroup();
+        this.traps.create(940, 567, 'level1_block_kill');
+        this.traps.create(940 + 32, 567, 'level1_block_lava');
+        this.traps.create(940 + 64, 567, 'level1_block_lava');
+        this.traps.create(940 + 96, 567, 'level1_block_lava');
+        this.traps.create(940 + 128, 567, 'level1_block_kill');
 
-        bombs = this.physics.add.group();
+        this.bombs = this.physics.add.group();
 
-        //  Collide the player and the stars with the platforms
-        this.physics.add.collider(player, platforms);
-        this.physics.add.collider(player, bossDoor);
-        this.physics.add.collider(stars, platforms);
-        this.physics.add.collider(bombs, platforms);
-        this.physics.add.collider(traps, platforms);
-        this.physics.add.collider(bossDoor, platforms);
+        //  Collide the this.player and the this.stars with the this.platforms
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.player, bossDoor);
+        this.physics.add.collider(this.stars, this.platforms);
+        this.physics.add.collider(this.bombs, this.platforms);
+        this.physics.add.collider(this.traps, this.platforms);
+        this.physics.add.collider(bossDoor, this.platforms);
 
-        //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-        this.physics.add.overlap(player, stars, this.collectStar, null, this);
+        //  Checks to see if the this.player overlaps with any of the this.stars, if he does call the collectStar function
+        this.physics.add.overlap(this.player, this.stars, this.collectStar, null, this);
 
-        this.physics.add.collider(player, bombs, this.hitBomb, null, this);
-        this.physics.add.collider(player, traps, this.hitTrap, null, this);
-        this.physics.add.collider(player, bossDoors, this.enterBoss, null, this);
+        this.physics.add.collider(this.player, this.bombs, this.hitBomb, null, this);
+        this.physics.add.collider(this.player, this.traps, this.hitTrap, null, this);
+        this.physics.add.collider(this.player, bossDoors, this.enterBoss, null, this);
 
-        // add camera to follow the player
-        camera = this.cameras.main;
-        camera.useBounds = true;
-        camera.setBounds(0, 0, config.physics.arcade.width, 768, false);
-        camera.startFollow(player);
-        camera.zoom         = 1.6;
-        camera.zoomGameplay = 1.6;
+        // add camera to follow the this.player
+        this.cameras.main.useBounds = true;
+        this.cameras.main.setBounds(0, 0, config.physics.arcade.width, 768, false);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.zoom         = 1.6;
+        this.cameras.main.zoomGameplay = 1.6;
 
-        scoreText = this.add.text(camera.centerX * (2 - camera.zoom), camera.centerY * (2 - camera.zoom), 'score: 0', { fontSize: '32px', fill: '#000' });
-        scoreText.setScrollFactor(0);
+        this.scoreText = this.add.text(this.cameras.main.centerX * (2 - this.cameras.main.zoom), this.cameras.main.centerY * (2 - this.cameras.main.zoom), 'score: 0', { fontSize: '32px', fill: '#000' });
+        this.scoreText.setScrollFactor(0);
     }
 
     update () {
 
-        if (gameOver) {
+        if (this.gameOver) {
             return;
         }
 
         // godmode on or off
-        if (cursors.F9.isDown) {
-            player.godMode    = true;
-            player.body.moves = false;
-            camera.zoom = player.godModeZoom;
-            scoreText.x = camera.centerX * 0.01;
-            scoreText.y = camera.centerY * 0.01;
+        if (this.cursors.F9.isDown) {
+            this.player.godMode    = true;
+            this.player.body.moves = false;
+            this.cameras.main.zoom = this.player.godModeZoom;
+            this.scoreText.x = this.cameras.main.centerX * 0.01;
+            this.scoreText.y = this.cameras.main.centerY * 0.01;
         }
-        if (cursors.F10.isDown) {
-            player.godMode    = false;
-            player.body.moves = true;
-            camera.zoom = camera.zoomGameplay;
-            scoreText.x = camera.centerX * (2 - camera.zoom);
-            scoreText.y = camera.centerY * (2 - camera.zoom);
+        if (this.cursors.F10.isDown) {
+            this.player.godMode    = false;
+            this.player.body.moves = true;
+            this.cameras.main.zoom = this.cameras.main.zoomGameplay;
+            this.scoreText.x = this.cameras.main.centerX * (2 - this.cameras.main.zoom);
+            this.scoreText.y = this.cameras.main.centerY * (2 - this.cameras.main.zoom);
         }
 
-        if (cursors.left.isDown || cursors.A.isDown) {
-            if (player.godMode) {
-                player.x -= player.godModeMoveSpeed * 5;
+        if (this.cursors.left.isDown || this.cursors.A.isDown) {
+            if (this.player.godMode) {
+                this.player.x -= this.player.godModeMoveSpeed * 5;
             }
 
-            player.setVelocityX(-560);
-            player.lastDirection = 'left';
-            player.anims.play('walk_left', true);
+            this.player.setVelocityX(-560);
+            this.player.lastDirection = 'left';
+            this.player.anims.play('walk_left', true);
         }
-        else if (cursors.right.isDown || cursors.D.isDown) {
-            if (player.godMode) {
-                player.x += player.godModeMoveSpeed * 5;
+        else if (this.cursors.right.isDown || this.cursors.D.isDown) {
+            if (this.player.godMode) {
+                this.player.x += this.player.godModeMoveSpeed * 5;
             }
 
-            player.setVelocityX(560);
-            player.lastDirection = 'right';
-            player.anims.play('walk_right', true);
+            this.player.setVelocityX(560);
+            this.player.lastDirection = 'right';
+            this.player.anims.play('walk_right', true);
         }
         else {
-            player.setVelocityX(0);
+            this.player.setVelocityX(0);
 
-            player.anims.play('idle_' + player.lastDirection, true);
+            this.player.anims.play('idle_' + this.player.lastDirection, true);
         }
 
-        if ( player.godMode ) {
-            if (cursors.up.isDown || cursors.W.isDown) {
-                player.y -= player.godModeMoveSpeed * 5;
+        if ( this.player.godMode ) {
+            if (this.cursors.up.isDown || this.cursors.W.isDown) {
+                this.player.y -= this.player.godModeMoveSpeed * 5;
             }
-            if (cursors.down.isDown || cursors.S.isDown) {
-                player.y += player.godModeMoveSpeed * 5;
+            if (this.cursors.down.isDown || this.cursors.S.isDown) {
+                this.player.y += this.player.godModeMoveSpeed * 5;
             }
         }
         else {
 
-            if ((cursors.up.isDown || cursors.W.isDown) && player.store.jumpCount > 0 && player.store.jumpPossible) {
-                if (player.store.jumpCount == 2) {
-                    player.store.velocityY = Math.min(player.store.jumpVelocityY, -1000);
-                    player.store.jumpVelocityY -= 1000;
+            if ((this.cursors.up.isDown || this.cursors.W.isDown) && this.player.store.jumpCount > 0 && this.player.store.jumpPossible) {
+                if (this.player.store.jumpCount == 2) {
+                    this.player.store.velocityY = Math.min(this.player.store.jumpVelocityY, -1000);
+                    this.player.store.jumpVelocityY -= 1000;
                 }
                 else {
-                    player.store.velocityY = Math.min(player.store.jumpVelocityY, -1500);
-                    player.store.jumpVelocityY -= 1500;
+                    this.player.store.velocityY = Math.min(this.player.store.jumpVelocityY, -1500);
+                    this.player.store.jumpVelocityY -= 1500;
                 }
-                player.store.jumpCount -= 1;
-                player.store.jumpPossible = false;
+                this.player.store.jumpCount -= 1;
+                this.player.store.jumpPossible = false;
             }
-            if ( !player.body.touching.down ) {
-                player.store.velocityY += 75;
-                player.store.jumpVelocityY += 75;
-                if ( player.store.jumpVelocityY > 0 ) {
-                    player.store.jumpVelocityY = 0;
+            if ( !this.player.body.touching.down ) {
+                this.player.store.velocityY += 75;
+                this.player.store.jumpVelocityY += 75;
+                if ( this.player.store.jumpVelocityY > 0 ) {
+                    this.player.store.jumpVelocityY = 0;
                 }
             }
-            if ( player.body.touching.down ) {
-                player.store.jumpCount = 2;
-                player.store.jumpPossible = true;
+            if ( this.player.body.touching.down ) {
+                this.player.store.jumpCount = 2;
+                this.player.store.jumpPossible = true;
             }
-            if ( !cursors.up.isDown && !cursors.W.isDown ) {
-                player.store.jumpPossible = true;
+            if ( !this.cursors.up.isDown && !this.cursors.W.isDown ) {
+                this.player.store.jumpPossible = true;
             }
 
-            player.setVelocityY(player.store.velocityY);
+            this.player.setVelocityY(this.player.store.velocityY);
         }
 
     }
@@ -239,14 +240,14 @@ class SceneLevel1 extends Phaser.Scene {
         star.disableBody(true, true);
 
         //  Add and update the score
-        score += 10;
-        if (player.godMode) score += 10000;
-        scoreText.setText('score: ' + score);
+        this.scoreCount += 10;
+        if (player.godMode) this.scoreCount += 10000;
+        this.scoreText.setText('score: ' + this.scoreCount);
 
-        if (stars.countActive(true) === 0)
+        if (this.stars.countActive(true) === 0)
         {
-            //  A new batch of stars to collect
-            stars.children.iterate(function (child) {
+            //  A new batch of this.stars to collect
+            this.stars.children.iterate(function (child) {
 
                 child.enableBody(true, child.x, 0, true, true);
 
@@ -254,7 +255,7 @@ class SceneLevel1 extends Phaser.Scene {
 
             var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
 
-            var bomb = bombs.create(x, 16, 'level1_bomb');
+            var bomb = this.bombs.create(x, 16, 'level1_bomb');
             bomb.setBounce(1);
             bomb.setCollideWorldBounds(true);
             bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
@@ -273,7 +274,7 @@ class SceneLevel1 extends Phaser.Scene {
 
         player.anims.play('turn');
 
-        gameOver = true;
+        this.gameOver = true;
     }
 
     hitBomb (player, bomb) {
@@ -285,12 +286,12 @@ class SceneLevel1 extends Phaser.Scene {
 
         player.anims.play('turn');
 
-        gameOver = true;
+        this.gameOver = true;
     }
 
     enterBoss(player, door) {
         if (player.godMode) return;
-        if (!cursors.up.isDown && !cursors.W.isDown) return;
+        if (!this.cursors.up.isDown && !this.cursors.W.isDown) return;
 
         this.level1Music.stop();
         this.scene.start("SceneLevel1Boss");
