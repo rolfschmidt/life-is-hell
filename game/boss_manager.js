2f6b365e89abb3ddaac86d7689abb900f17d8fae
Thorsten Eckel <te@znuny.com>
2019-10-06 19:15:54 +0200

    Ups. Added missing boss manager.

---
+++ game/boss_manager.js
@@ -0,0 +1,27 @@
+function BossManager() {}
+
+BossManager.prototype.preload = function(scene) {
+}
+
+BossManager.prototype.create = function(scene) {
+    scene.boss = scene.physics.add.sprite(300, 450, 'sprites', 'boss_attack_left');
+    scene.boss.setBounce(0.2);
+    scene.boss.setCollideWorldBounds(true);
+    scene.boss.anims.play('boss_attack_left', true);
+}
+
+BossManager.prototype.update = function(scene) {
+
+    var yLow  = 0;
+    var yHigh = 0;
+    if (scene.boss.x < scene.player.x) {
+        yLow = 50;
+        yHigh = 300;
+    }
+    else {
+        yLow = -300;
+        yHigh = -50;
+    }
+
+    scene.boss.setVelocity(Phaser.Math.Between(yLow, yHigh), 0);
+}
