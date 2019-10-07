e7963339d4b1ec481ba3b5b524c3abc12760f5cd
Rolf Schmidt <rolf@r-schmidt-gmbh.de>
2019-10-07 17:28:00 +0200

    Disabled boss.

--- game/boss_manager.js
+++ game/boss_manager.js
@@ -8,24 +8,25 @@ BossManager.prototype.create = function(scene) {
     scene.boss.setBounce(0.2);
     scene.boss.setCollideWorldBounds(true);
     scene.boss.anims.play('boss_attack_left', true);
+    scene.boss.visible = false;
 }
 
 BossManager.prototype.update = function(scene) {
-    if (scene.player.godMode) {
-        scene.boss.body.moves = false;
-        return;
-    }
+    // if (scene.player.godMode || config.devMode) {
+    //     scene.boss.body.moves = false;
+    //     return;
+    // }
 
-    var yLow  = 0;
-    var yHigh = 0;
-    if (scene.boss.x < scene.player.x) {
-        yLow = 50;
-        yHigh = 300;
-    }
-    else {
-        yLow = -300;
-        yHigh = -50;
-    }
+    // var yLow  = 0;
+    // var yHigh = 0;
+    // if (scene.boss.x < scene.player.x) {
+    //     yLow = 50;
+    //     yHigh = 300;
+    // }
+    // else {
+    //     yLow = -300;
+    //     yHigh = -50;
+    // }
 
-    scene.boss.setVelocity(Phaser.Math.Between(yLow, yHigh), 0);
+    // scene.boss.setVelocity(Phaser.Math.Between(yLow, yHigh), 0);
 }
